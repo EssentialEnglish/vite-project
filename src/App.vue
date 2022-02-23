18dede39dca2454f6dde37e6e51206c53629bd23
EssentialEnglish <essentialenglish@protonmail.com>
2022-02-23 21:33:10 +0700

    Firbase configured

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,48 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<!-- copied from learn view firbase auth lesson -->
-
-
 <template>
-  <h1 class="text-3xl font-bold underline">
-Setting up Firebase / What happened?
-  </h1>
-    <div>
+  <div>
     <nav>
       <router-link to="/"> Home </router-link> |
       <router-link to="/feed"> Feed </router-link> |
-      <router-link to="/register"> Register </router-link> |
-      <router-link to="/sign-in"> Login </router-link> |
+      <span v-if="isLoggedIn"
+        >>
+        <button @click="signOut">Logout</button>
+      </span>
+      <span v-else>
+        <router-link to="/register"> Register </router-link> |
+        <router-link to="/sign-in"> Login </router-link>
+      </span>
     </nav>
     <router-view />
   </div>
 </template>
 
+<script setup>
+  import { ref, watchEffect } from 'vue' // used for conditional rendering
+  import firebase from 'firebase'
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
+  const isLoggedIn = ref(true)
+  // runs after firebase is initialized
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        isLoggedIn.value = true // if we have a user
+      } else {
+        isLoggedIn.value = false // if we do not
+      }
+  })
+  const signOut = () => {
+    firebase.auth().signOut()
+    router.push('/')
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 // Copied from learn Vue - connect to firbase lesson
 import { createApp } from "vue";
 import App from "./App.vue";
-import './index.css'
 import router from "./router";
+import { initializeApp } from "firebase/app";
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBt6tGj1lY9O41prvQ4ez4LvWL5s-UqtK4",
+    authDomain: "vite-project-74ed0.firebaseapp.com",
+    projectId: "vite-project-74ed0",
+    storageBucket: "vite-project-74ed0.appspot.com",
+    messagingSenderId: "188074360383",
+    appId: "1:188074360383:web:7f76f4bae3fc3a2bfb428a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase
+
 
 const app = createApp(App);
 
